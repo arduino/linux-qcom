75224d55bd4f8280b5845f4ed1f9fe8266346ccb
Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>
2025-08-13 17:29:15 +0100

    ASoC: qcom: q6adm: the the copp device only during last instance
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>

--- sound/soc/qcom/qdsp6/q6adm.c
+++ sound/soc/qcom/qdsp6/q6adm.c
@@ -569,11 +569,12 @@ int q6adm_close(struct device *dev, struct q6copp *copp)
 {
 	struct q6adm *adm = dev_get_drvdata(dev->parent);
 	int ret = 0;
-
-	ret = q6adm_device_close(adm, copp, copp->afe_port, copp->copp_idx);
-	if (ret < 0) {
-		dev_err(adm->dev, "Failed to close copp %d\n", ret);
-		return ret;
+	if (kref_read(&copp->refcount) == 1) {
+		ret = q6adm_device_close(adm, copp, copp->afe_port, copp->copp_idx);
+		if (ret < 0) {
+			dev_err(adm->dev, "Failed to close copp %d\n", ret);
+			return ret;
+		}
 	}
 
 	kref_put(&copp->refcount, q6adm_free_copp);
